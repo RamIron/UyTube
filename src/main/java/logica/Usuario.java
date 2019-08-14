33e6445a846657d08ed53804fa241bfd66d63285
mateo.sayas <mateo.sayas@fing.edu.uy>
2019-08-14 11:35:55 -0300

    Clase usuario creada, solo con los atributos basicos (a modo de prueba)

---
+++ src/main/java/logica/Usuario.java
@@ -0,0 +1,15 @@
+package logica;
+
+import java.sql.Date;
+
+public class Usuario {
+	
+	private String nickname;
+	private String nombre;
+	private String apellido;
+	private Date fNac;
+	private String imagen;
+	private String correoE;
+//obviamente le faltan atributos
+}
+
