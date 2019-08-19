69cf77a5638eaff682ca5217f9e51b7e2e728f9b
mateo.sayas <mateo.sayas@fing.edu.uy>
2019-08-19 08:24:08 -0300

    factory, controlador e interfaz de ListaReproduccion. fix a: factory, controlador e interfaz de Video

--- src/main/java/interfaces/ICategoria.java
+++ src/main/java/interfaces/ICategoria.java
@@ -7,8 +7,11 @@ import datatypes.DtElementoUsuario;
 public interface ICategoria {
 
 	public boolean existeCategoria(String nomC);
+	
 	public void altaCategoria(String nomC);
+	
 	public ArrayList<DtElementoUsuario> listarElemCategoria(String nomC);
+	
 	public ArrayList<String> listarCategorias();
 	
 }
---
+++ src/main/java/interfaces/ILIstaReproduccion.java
@@ -0,0 +1,44 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+import datatypes.DtComentario;
+import datatypes.DtListaRep;
+import datatypes.DtValoracion;
+import datatypes.DtVideo;
+
+public interface ILIstaReproduccion {
+
+	public void agregarCategoriaALista(String nick, String nomL, String nomC);
+	
+	public void agregarListaDefecto(String nomL);
+	
+	public void agregarListaParticular(String nick, String nomL, boolean publico);
+	
+	public void agregarVideoLista(String nomVid, String nomList);
+	
+	public void eliminarVideoDeLista(String nickVid, String nomVid, String nomList);
+	
+	public boolean existeListaDefecto(String nomL);
+	
+	public boolean existeListaParticular(String nick, String nomL);
+	
+	public ArrayList<String> listarListasDeUsuario(String nick);
+	
+	public ArrayList<String> listarListasParticulares(String nick);
+	
+	public ArrayList<String> listarVideosdeLista(String nomList);
+	
+	public void modificarCategoria(String nick, String nomL, String nomC);
+	
+	public void modificarInfoLista(String nick, String nomL, boolean publico);
+	
+	public ArrayList<DtComentario> obtenerComentariosVideo(String nomVid);
+	
+	public DtVideo obtenerInfoVideo(String nomVid);
+	
+	public DtListaRep obtenerListaDeUsuario(String nomList);
+	
+	public ArrayList<DtValoracion> obtenerValoracionVideo(String nomVid);
+	
+}
