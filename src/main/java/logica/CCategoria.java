a2ac4ea9242ea37e75910b65f0c93b8a1b406db0
mateo.sayas <mateo.sayas@fing.edu.uy>
2019-08-18 23:17:53 -0300

    Interfaz, fabrica y controlador Categoria. Interfaz, fabrica y controlador de video (con errores)

---
+++ src/main/java/logica/CCategoria.java
@@ -0,0 +1,16 @@
+package logica;
+
+import java.util.ArrayList;
+import datatypes.DtElementoUsuario;
+import interfaces.ICategoria;
+
+public class CCategoria implements ICategoria {
+	public Categoria cat;
+	
+	//Operaciones
+	public boolean existeCategoria(String nomC) {}
+	public void altaCategoria(String nomC) {}
+	public ArrayList<DtElementoUsuario> listarElemCategoria(String nomC) {}
+	public ArrayList<String> listarCategorias() {}
+	
+}
