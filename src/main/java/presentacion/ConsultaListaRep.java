bb6a00fdcc109a7c21dbc29172e5152a7cd62390
tincho <martin.navarrete@fing.edu.uy>
2019-09-01 19:39:55 -0300

    recin empiezo

---
+++ src/main/java/presentacion/ConsultaListaRep.java
@@ -0,0 +1,15 @@
+package presentacion;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class ConsultaListaRep extends JInternalFrame {
+
+
+	public ConsultaListaRep() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
