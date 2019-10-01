b91023b7cc43bb5b5e0003c33c96db3f60785f72
martin.navarrete <martin.navarrete@fing.edu.uy>
2019-10-01 17:10:55 -0300

    logica servlet seguir y dejar seguir funcionando

---
+++ UyTubeWeb/src/main/java/servlets/DejarSeguirUsuario.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import datatypes.DtUsuarioWeb;
+import interfaces.IUsuario;
+import interfaces.UFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "DejarSeguirUsuario", value = "/DejarSeguirUsuario")
+public class DejarSeguirUsuario extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String seguido = request.getParameter("u");
+        HttpSession s = request.getSession();
+        DtUsuarioWeb usr = (DtUsuarioWeb) s.getAttribute("usuario");
+        if (seguido != null && usr != null){
+            String seguidor = usr.getNickname();
+            UFactory f = UFactory.getInstancia();
+            IUsuario iU = f.getIUsuario();
+            iU.dejarDeSeguirUsuario(seguidor, seguido);
+            RequestDispatcher rd;
+            String path = "/module/consultaUsuario.jsp?u=" + seguido;
+            rd = request.getRequestDispatcher(path);
+            String message = "Se dejo seguir al usuario " + seguido;
+            request.setAttribute("message", message);
+            rd.forward(request, response);
+        } else {
+            response.getWriter().append("Parametros invalidos");
+        }
+    }
+}
